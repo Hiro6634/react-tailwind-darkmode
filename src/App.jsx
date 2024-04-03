f06a76072846533e126044763d8dfbffd237d898
Hiro Suyama <hiro.suyama@gmail.com>
2024-04-03 00:00:35 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return(
+    <div className="h-screen flex justify-center items-center">
+      <button className="bg-slate-200 px-4 py-2 rounded">
+        Change Theme
+      </button>
+    </div>
+  )
+}
+
+export default App
